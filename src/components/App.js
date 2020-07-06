4e52b2e0fd46ae502ba42bf16c5e72ae4f22621f
N-E-K-0 <samiul.islam94@hotmail.com>
2020-07-06 13:12:53 +0600

    404 html change

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ import {BrowserRouter , Switch, Route} from 'react-router-dom'
 function App() {
   return (
     <React.Fragment>
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <Header />
         <Switch>
           <Route exact path="/Portfolio" component={Home}/>
