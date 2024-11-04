a8f94a97769c59b9540e6d4084f4df117fc90620
N-E-K-0 <samiul.islam94@hotmail.com>
2024-11-04 20:10:33 +0600

    add:  mobile apps section modification

--- src/components/MobileApps.js
+++ src/components/MobileApps.js
@@ -23,12 +23,12 @@ export default function MobileApps() {
             <a
               href={project.link}
               key={project.image}
-              className="sm:w-1/2 w-100 h-fit p-4"
+              className="sm:w-1/2 w-100 p-4"
             >
               <div className="flex relative">
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-fit object-cover object-center"
+                  className="absolute inset-0 w-full object-cover object-center"
                   src={project.image}
                 />
                 <div className="px-8 py-10 relative z-10 w-full h-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
