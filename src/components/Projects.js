3a616ba1f534fa487c0e44b12826178bda2f4480
N-E-K-0 <samiul.islam94@hotmail.com>
2024-11-04 17:58:42 +0600

    fix: mobile apps section height

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -66,7 +66,7 @@ export default function Projects() {
               <div className="flex relative">
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-[500px] object-cover object-center"
+                  className="absolute inset-0 w-full h-fit object-cover object-center"
                   src={project.image}
                 />
                 <div className="px-8 py-10 relative z-10 w-full h-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
