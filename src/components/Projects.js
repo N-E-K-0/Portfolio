8f77ece405d688bc39f86bf4ffb9acd3473a645e
N-E-K-0 <samiul.islam94@hotmail.com>
2024-11-04 17:25:21 +0600

    fix: mobile app section height adjusted

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -44,23 +44,24 @@ export default function Projects() {
           ))}
         </div>
 
-        <div className="flex flex-col w-full mb-20">
+        {/* mobile apps */}
+        <div className="flex flex-col h-full mb-20">
           <CodeIcon className="mx-auto inline-block w-10 mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
             Mobile Apps I've Built
           </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+          <p className="lg:h-2/3 mx-auto leading-relaxed text-base">
             I have build the websites using HTML, CSS, JavaScript and React
             Native.
           </p>
         </div>
-        {/* mobile apps */}
+
         <div className="flex flex-wrap -m-4">
           {mobile_apps.map((project) => (
             <a
               href={project.link}
               key={project.image}
-              className="sm:w-1/2 w-100 p-4"
+              className="sm:h-1/2 h-100 p-4"
             >
               <div className="flex relative">
                 <img
@@ -68,7 +69,7 @@ export default function Projects() {
                   className="absolute inset-0 w-full h-full object-cover object-center"
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                <div className="px-8 py-10 relative z-10 h-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
